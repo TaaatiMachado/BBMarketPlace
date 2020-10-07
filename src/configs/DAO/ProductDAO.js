75af139295a77ce33e132944ccbd63383cc23a10
Fabinnreis <fabioreisti@outlook.com>
2020-10-07 11:21:20 -0300

    Update Product DAO

--- src/configs/DAO/ProductDAO.js
+++ src/configs/DAO/ProductDAO.js
@@ -4,6 +4,9 @@ class ProductDAO{
         this._getProducts = 'SELECT * FROM PRODUCTS';
         this._getProductsBySupplier = 'SELECT * FROM PRODUCTS INNER JOIN PROVIDERS ON id_provider = id'
         this._getProductBySupplier = 'SELECT * FROM PRODUCTS INNER JOIN PROVIDERS ON id_provider = id WHERE id_product = ?'
+        this._insertProduct = 'INSERT INTO PRODUCTS (evaluation, name, description, price, quantity, id_provider) VALUES (?,?,?,?,?,?)';
+        this._modifyProduct = 'UPDATE PRODUCTS SET evaluation = ?, name = ?, description = ?, price = ?, quantity = ?, id_provider = ? WHERE id_product = ?';
+        this._deleteProduct = 'DELETE FROM PRODUCTS WHERE id_PRODUCT = ?'
     }
 
     getProducts(){
@@ -36,6 +39,40 @@ class ProductDAO{
                     reject(`Error getting products: ${err}`)
                 }
                     resolve(rows)
+            })
+        })
+    }
+    insertProduct(req){
+        const {evaluation, name, description, price, quantity, id_provider}  = req.body;
+        return new Promise((resolve, reject)=>{
+            this._db.run(this._insertProduct, [evaluation, name, description, price, quantity, id_provider], (err)=>{
+                if(err){
+                    reject(`Error inserting product: ${err}`)
+                }
+                    resolve('Success!')
+            })
+        })
+    }
+
+    modifyProduct(req){
+        const {id_product, evaluation, name, description, price, quantity, id_provider}  = req.body;
+        return new Promise((resolve, reject)=>{
+            this._db.run(this._modifyProduct, [evaluation, name, description, price, quantity, id_provider, id_product], (err)=>{
+                if(err){
+                    reject(`Error modifying product: ${err}`)
+                }
+                    resolve('Success!')
+            })
+        })
+    }
+    deleteProduct(req){
+        const {id_product} = req.body;
+        return new Promise((resolve, reject)=>{
+            this._db.run(this._deleteProduct, [id_product], (err)=>{
+                if(err){
+                    reject(`Error deleting product: ${err}`)
+                }
+                    resolve('Success!')
             })
         })
     }
